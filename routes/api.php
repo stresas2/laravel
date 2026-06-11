e613e688607bf9911df1b54b9d56be6704d69241
Lukas <lukas.budrys@skycop.com>
2026-06-11 17:15:42 +0300

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,3 @@ use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/{sku}', [ProductController::class, 'show']);
